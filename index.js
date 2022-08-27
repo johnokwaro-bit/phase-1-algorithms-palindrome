39db5f6979db0df16c2dc49bb1a60f64e2e6dd64
John Okwaro <john.okwaro@moringaschool.com>
2022-08-28 00:38:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //define a regular expression
+  let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+ // remove punctuation and convert to lower case.
+ word = word.replace(regex, '').toLowerCase();
+ // Compare the string with it's reversed version in a if-statement.
+ if (word === word.split('').reverse().join('')){
+  return true;
+ }
+ else 
+ return false;
+
 }
 
 /* 
   Add your pseudocode here
-*/
+//define a regular expression contatining non-alphanumeric
+//remove non-alphanumeric characters
+//convert to lower case
+//compare string with its reversed in a if-statement
+
+
 
 /*
   Add written explanation of your solution here
